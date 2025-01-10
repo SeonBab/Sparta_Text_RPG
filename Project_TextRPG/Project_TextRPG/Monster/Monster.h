422ea5b5ab1cb1df4c8fa870e9dc63d759fbd4f0
F1ANK76 <ygodlksg@gmail.com>
2025-01-10 20:53:08 +0900

    Update Monster.h

--- Project_TextRPG/Project_TextRPG/Monster/Monster.h
+++ Project_TextRPG/Project_TextRPG/Monster/Monster.h
@@ -1,42 +1,22 @@
-﻿#pragma once
+// Monster.h
+#pragma once
 
-#include "Framework/Framework.h"
+#include "../Framework/Framework.h"
 
-enum class EMonsterType
-{
-    A,
-    B,
-    C,
-    End
-};
-
-extern string Strings[(unsigned int)EMonsterType::End];
-
-class MonsterDef : public Entity
-{
-    
+class Monster : public Entity {
 public:
+    // Entity 생성자 (구현 필요?)
+    Monster(string name, int maxHP, int damage);
 
-    MonsterDef();
+    // 생성자 : 플레이어 및 난이도에 따른 몬스터 생성
+    Monster(int player_level, int difficulty);
 
-    void GenerataeLevel();
-    
-    void Display();
+    // 데미지 처리 함수 : 입력값 만큼 몬스터 체력 차감
+    void TakeDamage(int Damage);
+
+    // difficulty 값을 반환하는 Getter 함수
+    int GetDifficulty() const { return difficulty; }
 
 private:
-
-    int MaxLevel;
-    int Level;
-    
-};
-
-class Monster : public Entity
-{
-    
-public:
-    
-    // Def로 생성 로직 추가
-    Monster(const MonsterDef& Def) : Entity("None", 1, 1)
-    {
-    }
+    int difficulty;  // 난이도 값을 저장하는 멤버 변수
 };
