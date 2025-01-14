739b33c4876512b244918034c45aff4f26da2af0
cheeztabby <84297394+Cheeztabby@users.noreply.github.com>
2025-01-14 16:03:02 +0900

    Update BattleManager.cpp
    커밋 오류 수정

--- Project_TextRPG/Project_TextRPG/Framework/Managers/BattleManager.cpp
+++ Project_TextRPG/Project_TextRPG/Framework/Managers/BattleManager.cpp
@@ -35,17 +35,13 @@ bool BattleManager::Battle(Monster* SelectedMonster, Player* Player)
     {
         // 플레이어 클래스의 인벤토리에 직접 접근하는 것으로 변경됨
         auto Items = ItemList::GetInstance().GetItems();
-        <<<<<< < Updated upstream
-
-            // ItemList에서 랜덤으로 하나 사용
-            const int RandItemIdx = RandRange(0, Items.size());
+        // ItemList에서 랜덤으로 하나 사용
+        const int RandItemIdx = RandRange(0, Items.size());
         Items[RandItemIdx]->Use();
-        ====== =
-            // Items 클래스에 있는 종류중에 한가지 사용
-            int ItemType = RandRange(0, Items.size() - 1);
+        // Items 클래스에 있는 종류중에 한가지 사용
+        int ItemType = RandRange(0, Items.size() - 1);
         Player->UseItem(Items[ItemType].get()->GetName());
 
-        >>>>>> > Stashed changes
     }
     while (true)
     {
