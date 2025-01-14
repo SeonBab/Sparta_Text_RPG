8dddfa617aa8c6f1b1e4b28d0909baf592ce2e41
SeonGuk Kim <tjsrnr3180@gmail.com>
2025-01-14 14:02:37 +0900

    fix 아이템과 상점 기능 수정
    아이템이 어떻게 사용될지에 대한 값 설정
    아이템의 사용 구현
    상점의 전체적인 출력 수정
    상점의 판매 기능 추가

--- Project_TextRPG/Project_TextRPG/Item.cpp
+++ Project_TextRPG/Project_TextRPG/Item.cpp
@@ -1,1 +1,39 @@
-#include "Item.h"
+﻿#include "Item.h"
+#include "Player/Player.h"
+
+void HealthPotion::Use()
+{
+    // 체력 회복 물약이 회복시킬 값
+    int HPRecovery = 5;
+
+    // 플레이어 가져오기
+    Player* CurPlayer = Player::GetInstance();
+    if (nullptr == CurPlayer)
+    {
+        return;
+    }
+
+    int CurHP = CurPlayer->GetHP();
+
+    // HP를 회복시키려 할 때 최대 체력보다 높아지지 않도록 설정
+    CurHP = (CurHP + HPRecovery > CurPlayer->GetMaxHP()) ? CurPlayer->GetMaxHP() : CurHP + HPRecovery;
+
+    CurPlayer->SetHP(CurHP);
+}
+
+void AttackBoost::Use()
+{
+    // 공격력 부스트가 증가시킬 공격력 값
+    int DamageUp = 10;
+
+    // 플레이어 가져오기
+    Player* CurPlayer = Player::GetInstance();
+    if (nullptr == CurPlayer)
+    {
+        return;
+    }
+
+    DamageUp = CurPlayer->GetDamage() + DamageUp;
+
+    CurPlayer->SetDamage(DamageUp);
+}
