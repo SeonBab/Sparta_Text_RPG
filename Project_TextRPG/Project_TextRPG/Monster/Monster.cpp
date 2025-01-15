3b060cdcddd1796e0139431fd5ccb9827561af93
F1ANK76 <ygodlksg@gmail.com>
2025-01-15 14:51:48 +0900

    몬스터 관련 변수명 수정

--- Project_TextRPG/Project_TextRPG/Monster/Monster.cpp
+++ Project_TextRPG/Project_TextRPG/Monster/Monster.cpp
@@ -3,38 +3,38 @@
 #include <iostream>
 
 // Entity의 생성자를 호출하는 Monster 생성자
-Monster::Monster(std::string name, int HP, int damage, int difficulty)
-    : Entity(name, HP, damage), difficulty(difficulty) {
+Monster::Monster(string Name, int Hp, int Damage, int Difficulty)
+    : Entity(Name, Hp, Damage), Difficulty(Difficulty) {
 }
 
-// 몬스터 이름, HP, Damage 값을 생성하는 함수
-MonsterInfo Monster::CreateMonsterInfo(int player_level, int difficulty) {
-    MonsterInfo info;
+// 몬스터 이름, 체력, 공격력 값을 생성하는 함수
+FMonsterInfo Monster::CreateMonsterInfo(int PlayerLevel, int Difficulty) {
+    FMonsterInfo MonsterInfo;
 
-    if (difficulty == 4) { // 보스 몬스터
-        info.name = "Boss";
-        info.hp = (rand() % 16 + 30) * player_level;  // (30~45) * 플레이어 레벨
-        info.damage = (rand() % 6 + 8) * player_level;    // (8~13) * 플레이어 레벨
+    if (Difficulty == 4) { // 보스 몬스터
+        MonsterInfo.MonsterName = "Boss";
+        MonsterInfo.MonsterHp = (rand() % 16 + 30) * PlayerLevel;  // (30~45) * 플레이어 레벨
+        MonsterInfo.MonsterDamage = (rand() % 6 + 8) * PlayerLevel;    // (8~13) * 플레이어 레벨
     }
     else { // 일반 몬스터
-        if (difficulty == 1) { // 쉬움
-            info.name = "Slime";
-            info.hp = (rand() % 11 + 20) * player_level;  // (20~30) * 플레이어 레벨
-            info.damage = (rand() % 6 + 5) * player_level;    // (5~10) * 플레이어 레벨
+        if (Difficulty == 1) { // 쉬움 난이도 몬스터
+            MonsterInfo.MonsterName = "Slime";
+            MonsterInfo.MonsterHp = (rand() % 11 + 20) * PlayerLevel;  // (20~30) * 플레이어 레벨
+            MonsterInfo.MonsterDamage = (rand() % 6 + 5) * PlayerLevel;    // (5~10) * 플레이어 레벨
         }
-        else if (difficulty == 2) { // 보통
-            info.name = "Goblin";
-            info.hp = (rand() % 11 + 22) * player_level;  // (22~32) * 플레이어 레벨
-            info.damage = (rand() % 6 + 6) * player_level;    // (6~11) * 플레이어 레벨
+        else if (Difficulty == 2) { // 보통 난이도 몬스터
+            MonsterInfo.MonsterName = "Goblin";
+            MonsterInfo.MonsterHp = (rand() % 11 + 22) * PlayerLevel;  // (22~32) * 플레이어 레벨
+            MonsterInfo.MonsterDamage = (rand() % 6 + 6) * PlayerLevel;    // (6~11) * 플레이어 레벨
         }
-        else if (difficulty == 3) { // 어려움
-            info.name = "Oak";
-            info.hp = (rand() % 11 + 25) * player_level;  // (25~35) * 플레이어 레벨
-            info.damage = (rand() % 6 + 7) * player_level;    // (7~12) * 플레이어 레벨
+        else if (Difficulty == 3) { // 어려움 난이도 몬스터
+            MonsterInfo.MonsterName = "Oak";
+            MonsterInfo.MonsterHp = (rand() % 11 + 25) * PlayerLevel;  // (25~35) * 플레이어 레벨
+            MonsterInfo.MonsterDamage = (rand() % 6 + 7) * PlayerLevel;    // (7~12) * 플레이어 레벨
         }
     }
 
-    return info; // 계산된 몬스터 정보 반환
+    return MonsterInfo; // 계산된 몬스터 정보 반환
 }
 
 // 몬스터 데미지 처리 함수
--- Project_TextRPG/Project_TextRPG/Monster/Monster.h
+++ Project_TextRPG/Project_TextRPG/Monster/Monster.h
@@ -4,26 +4,27 @@
 #include "Framework/Framework.h"
 
 // 몬스터 정보를 담을 구조체
-struct MonsterInfo {
-    string name;
-    int hp;
-    int damage;
+struct FMonsterInfo {
+    string MonsterName;
+    int MonsterHp;
+    int MonsterDamage;
 };
 
 class Monster : public Entity {
 public:
     // Entity의 생성자를 호출하는 Monster 생성자
-    Monster(string name, int HP, int damage, int difficulty);
+    Monster(string Name, int Hp, int Damage, int Difficulty);
 
     // 몬스터 이름, HP, Damage 값을 생성하는 함수
-    static MonsterInfo CreateMonsterInfo(int player_level, int difficulty);
+    static FMonsterInfo CreateMonsterInfo(int PlayerLevel, int Difficulty);
 
     // 데미지 처리 함수 : 입력값 만큼 몬스터 체력 차감
     void TakeDamage(int Damage);
 
-    // difficulty 값을 반환하는 Getter 함수
-    int GetDifficulty() const { return difficulty; }
+    // Difficulty 값을 반환하는 Getter 함수
+    int GetDifficulty() const { return Difficulty; }
 
-    int difficulty;  // 난이도 값을 저장하는 멤버 변수
+    // 난이도 값을 저장하는 멤버 변수
+    int Difficulty;  
 };
 
